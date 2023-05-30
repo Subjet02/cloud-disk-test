5649c38d348b8abd1bb398f57b202ca3f4f76440
Subjet02 <130770820+Subjet02@users.noreply.github.com>
2023-05-30 22:42:57 +0300

    x

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,8 @@ const {Schema, model} = require("mongoose")
 const User = new Schema({
     email : {type: String, require: true, unique: true},
     password : {type: String, require: true},
-    diskSpace : {type: Number, default: 1024**3*10},
-    usedSpace : {type: String, default: 0},
+    diskSpace: {type: Number, default: 1024**3*10},
+    usedSpace: {type: Number, default: 0},
     avatar: {type: String},
     files : [{type: Object, ref:'File'}]
 })
